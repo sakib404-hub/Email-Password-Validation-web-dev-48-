05e574998ab1227dcfd3b0eccb7cbae6e0b2d92e
Md Sakib hossen <akibhossainsakib1040@gmail.com>
2025-10-21 01:54:14 +0600

    added the routes to login and register page

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,9 +1,43 @@
 import React from 'react';
+import { NavLink } from 'react-router';
 
 const Login = () => {
+
+
+    const handleLoginInformation = () => {
+        console.log('Button is Clicked')
+    }
     return (
         <div>
-            This is the Login Page!
+            <div className="hero bg-base-200 min-h-screen">
+                <div className="hero-content flex-col">
+                    <div className="text-center lg:text-left">
+                        <h1 className="text-5xl font-bold text-center">Login now!</h1>
+                    </div>
+                    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                        <div className="card-body space-y-3">
+                            <form
+                                onSubmit={handleLoginInformation}>
+                                <fieldset className="fieldset">
+                                    <label className="label">Email</label>
+                                    <input type="email" className="input" placeholder="Email" />
+                                    <label className="label">Password</label>
+                                    <input type="password" className="input" placeholder="Password" />
+                                    <div><a className="link link-hover">Forgot password?</a></div>
+                                    <button className="btn btn-neutral mt-4">Login</button>
+                                    <div className='flex items-center justify-around px-2'>
+                                        <p>Don't Have an Account?</p>
+                                        <NavLink
+                                            to='/register'
+                                            className='text-blue-400 underline font-medium'>Register</NavLink>
+
+                                    </div>
+                                </fieldset>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,23 +1,48 @@
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import React, { useState } from 'react';
 import { auth } from '../../Firebase/firebase.config';
+import { FaEye } from "react-icons/fa";
+import { FaLock } from "react-icons/fa";
+import { NavLink } from 'react-router';
 
 const Register = () => {
 
     const [error, setError] = useState('');
     const [success, setSuccess] = useState(false)
+    const [showPass, setShowPass] = useState(false);
 
     // handler for the form On submit 
     const handleFormOnSubmit = (e) => {
         e.preventDefault();
         const email = e.target.email.value;
         const password = e.target.password.value;
+        const terms = e.target.terms.checked;
+        console.log(terms);
         // console.log(email);
         // console.log(password);
         // console.log('Button is Clicked!')
 
         setError('');
         setSuccess(false);
+
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,12}$/;
+
+        if (!terms) {
+            setError('Please Accept Our Terms and Condition');
+            return;
+        }
+
+        if (!emailRegex.test(email)) {
+            setError('Kindly Enter a Valid Email Address!');
+            return;
+        }
+        if (!passwordRegex.test(password)) {
+            setError('Kindly Include Strong Password!');
+            return;
+        }
+
 
         createUserWithEmailAndPassword(auth, email, password)
             .then((result) => {
@@ -57,18 +82,37 @@ const Register = () => {
                                         name='email'
                                         id='email'
                                         required />
-                                    <label
-                                        className="label" htmlFor='password'>Password
-                                    </label>
-                                    <input
-                                        type="password"
-                                        className="input" placeholder="Password"
-                                        name='password'
-                                        id='password'
-                                        minLength={8}
-                                        maxLength={12}
-                                        required />
-                                    <div><a className="link link-hover">Forgot password?</a></div>
+                                    <div className='relative'>
+                                        <label
+                                            className="label" htmlFor='password'>Password
+                                        </label>
+                                        <input
+                                            type={showPass ? 'text' : 'password'}
+                                            className="input" placeholder="Password"
+                                            name='password'
+                                            id='password'
+                                            minLength={8}
+                                            maxLength={12}
+                                            required />
+                                        <div className='absolute right-6 top-7 text-xl'
+                                            onClick={() => setShowPass(!showPass)}>
+                                            {
+                                                showPass ? <FaLock /> : <FaEye />
+                                            }
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <fieldset className=" w-64 p-2">
+                                            <label className="label">
+                                                <input type="checkbox" className="checkbox"
+                                                    name='terms' />
+                                                Accept Terms and Conditions.
+                                            </label>
+                                        </fieldset>
+                                    </div>
+                                    <div>
+                                        <a className="link link-hover">Forgot password?</a>
+                                    </div>
                                     <button className="btn btn-neutral mt-4">Register</button>
                                 </fieldset>
                                 {
@@ -79,6 +123,12 @@ const Register = () => {
                                     error && <p
                                         className='text-red-500'>{error}</p>
                                 }
+                                <div className='flex items-center justify-around px-2'>
+                                    <p>Already Have an Account?</p>
+                                    <NavLink
+                                        to='/login'
+                                        className='text-blue-400 underline font-medium'>Login</NavLink>
+                                </div>
 
                             </div>
                         </div>
